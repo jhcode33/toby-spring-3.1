d2b0a39c9b1c774393402e2a9aa0b5a2b8b9fd48
jhcode33 <jhcode33@gmail.com>
2023-06-06 02:39:59 +0900

    템플릿/콜백 설계와 테스트

--- src/com/jhcode/spring/ch3/learningtest/template/CalcSumTest.java
+++ src/com/jhcode/spring/ch3/learningtest/template/CalcSumTest.java
@@ -4,17 +4,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CalcSumTest {
 	
+	Calculator calculator;
+	String numFilepath;
+	
+	//파일 이름과 사용되는 객체가 테스트마다 중복되기 때문에 테스트 전에 실행하는 @BeforeEach 어노테이션으로 따로 뺴두었다.
+	@BeforeEach
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
+	}
+	
+	
 	@Test
 	public void sumOfNumbers() throws IOException {
-		
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-		
-		assertEquals(10, sum);
+		assertEquals(10, calculator.calcSum(numFilepath));
 	}
 
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertEquals(24, calculator.calcMultiply(numFilepath));
+	}
 }
--- src/com/jhcode/spring/ch3/learningtest/template/Calculator.java
+++ src/com/jhcode/spring/ch3/learningtest/template/Calculator.java
@@ -8,16 +8,63 @@ public class Calculator {
 	
 	public Integer calcSum(String filepath) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		Integer sum = 0;
-		String line = null;
-		
-		while((line = br.readLine()) != null) { //마지막 라인까지 한줄씩 읽어가면서 숫자를 더한다.
-			sum += Integer.valueOf(line);
+		//콜백 오브젝트, 익명 내부 클래스이다.
+		BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
 			
-		}
+			//변경되는 부분, doSomethingWithReader을 오버라이드하여 재정의하여 콜백 동작을 수행한다.
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException{
+				
+				Integer sum = 0;
+				String line = null;
+				while((line = br.readLine()) != null) {
+					sum += Integer.valueOf(line);
+				}
+				
+				return sum;
+			}
+		};
+		return fileReadTemplate(filepath, sumCallback);
+	}
+	
+	
+	public Integer calcMultiply(String filePath) throws IOException {
 		
-		br.close();
-		return sum;
+		//콜백 오브젝트
+		BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+			
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer multiply = 1;
+				String line = null;
+				while((line = br.readLine()) != null) {
+					multiply *= Integer.valueOf(line);
+				}
+				return multiply;
+			}
+		};
+		return fileReadTemplate(filePath, multiplyCallback);
+	}
+	
+	
+	
+	//템플릿, -> 변경되지 않는 부분
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
+		
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+			
+		} finally {
+			if (br != null) {
+				try {br.close();} 
+				catch (IOException e) {System.out.println(e.getMessage());}
+			}
+		}
 	}
 }
