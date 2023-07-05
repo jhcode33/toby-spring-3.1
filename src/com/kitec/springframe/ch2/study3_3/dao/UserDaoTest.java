b09936a318a90f510cd749d423295832d57f31b4
jhcode33 <jhcode33@gmail.com>
2023-07-05 12:32:11 +0900

    add study2-3-3

---
+++ src/com/kitec/springframe/ch2/study3_3/dao/UserDaoTest.java
@@ -0,0 +1,59 @@
+package com.kitec.springframe.ch2.study3_3.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kitec.springframe.ch2.study3_3.domain.User;
+
+public class UserDaoTest {	
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {				
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		dao.deleteAll();	
+		assertEquals(dao.getCount(), 0);
+		
+		User user = new User();
+		user.setId("gyumee");
+		user.setName("�ڼ�ö");
+		user.setPassword("springno1");
+
+		dao.add(user);
+		assertEquals(dao.getCount(), 1);
+		
+		User user2 = dao.get(user.getId());		
+		
+		assertEquals(user2.getName(), user.getName());
+		assertEquals(user2.getPassword(), user.getPassword());		
+	}   
+	
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user1 = new User("gyumee", "gyumee", "springno1");
+		User user2 = new User("leegw700", "leegw700", "springno2");
+		User user3 = new User("bumjin", "bumjin", "springno3");
+		
+		dao.deleteAll();
+		assertEquals(dao.getCount(), 0);
+
+		dao.add(user1);
+		assertEquals(dao.getCount(), 1);
+		
+		dao.add(user2);
+		assertEquals(dao.getCount(), 2);
+		
+		dao.add(user3);
+		assertEquals(dao.getCount(), 3);	
+		
+	}
+
+}
