536526b0329b1b6e57d1dac7774dd12a2e7f312c
jhcode33 <jhcode33@gmail.com>
2023-07-05 13:48:10 +0900

    add study ch2-3-5

---
+++ src/com/kitec/springframe/ch2/study3_5/dao/UserDaoTest.java
@@ -0,0 +1,77 @@
+package com.kitec.springframe.ch2.study3_5.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.kitec.springframe.ch2.study3_5.domain.User;
+
+public class UserDaoTest { 
+	
+	private UserDao dao;
+	
+	private User user1;
+	private User user2;
+	private User user3;	
+	
+	// 각 단위 테스트가 실행되기 전에 먼저 호출된다
+	// 이를 픽스처(fixture) 테스트를 수행하는 데 필요한 정보나 오브젝트라고 한다
+	@BeforeEach
+	public void setUp() {	
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		this.dao = context.getBean("userDao", UserDao.class);
+		user1 = new User("user1", "sungkim", "5678");
+		user2 = new User("user2", "brucelee", "9012");
+		user3 = new User("user3", "haechoi", "1234");
+	}
+	
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException {				
+		dao.deleteAll();
+		assertEquals(dao.getCount(), 0);
+		
+		dao.add(user1);
+		dao.add(user2);
+		assertEquals(dao.getCount(), 2);
+		
+		User userget1 = dao.get(user1.getId());
+		assertEquals(user1.getName(), userget1.getName());
+		assertEquals(user1.getPassword(), userget1.getPassword());
+		
+		User userget2 = dao.get(user2.getId());		
+		assertEquals(user2.getName(), userget2.getName());
+		assertEquals(user2.getPassword(), userget2.getPassword());		
+	}
+	
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {		
+		dao.deleteAll();
+		assertEquals(dao.getCount(), 0);
+
+		dao.add(user1);
+		assertEquals(dao.getCount(), 1);
+		
+		dao.add(user2);
+		assertEquals(dao.getCount(), 2);
+		
+		dao.add(user3);
+		assertEquals(dao.getCount(), 3);		
+	}
+	
+	@Test
+	public void getUserFailure() throws SQLException, ClassNotFoundException {		
+		dao.deleteAll();
+		assertEquals(dao.getCount(), 0);		
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, 
+				() -> {dao.get("unknown_id");});	
+	}	
+
+}
