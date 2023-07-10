56d35e91480f2ea4f180f9044cb1cb1bb7cac574
jhcode33 <jhcode33@gmail.com>
2023-07-10 21:38:20 +0900

    add ch6 & refactoring upgradeLevels()

---
+++ src/com/jhcode/spring/ch6/user/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.jhcode.spring.ch6.user.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.jhcode.spring.ch6.user.domain.User;
+
+public interface UserDao {
+	void add(User user);
+
+	Optional<User> get(String id);
+
+	List<User> getAll();
+
+	void deleteAll();
+
+	int getCount();
+	
+	public void update(User user);
+}
---
+++ src/com/jhcode/spring/ch6/user/service/TestServiceFactory.java
@@ -0,0 +1,60 @@
+package com.jhcode.spring.ch6.user.service;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.jhcode.spring.ch6.user.dao.UserDao;
+import com.jhcode.spring.ch6.user.dao.UserDaoJdbc;
+
+@Configuration
+public class TestServiceFactory {
+	
+	@Bean
+	public UserDao userDao() {
+		UserDaoJdbc userDao = new UserDaoJdbc();
+		userDao.setDataSource(dataSource());
+		return userDao;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		String url = "jdbc:mariadb://localhost:3306/toby_study?characterEncoding=UTF-8";
+		String username = "root";
+		String password = "1234";
+		
+		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+	
+	@Bean
+	public UserService userService() {
+		UserService userService = new UserService();
+		userService.setUserDao(userDao());
+		userService.setTransactionManager(transactionManager());
+		//<property name="mailSender" ref="mailSender" />
+		userService.setMailSender(mailSender());
+		//userService.setDataSource(dataSource());
+		return userService;
+	}
+	
+	@Bean
+	public DummyMailSender mailSender() {
+		DummyMailSender dummyMailSender = new DummyMailSender();
+		return dummyMailSender;
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+		dataSourceTransactionManager.setDataSource(dataSource());
+		return dataSourceTransactionManager;
+	}
+}
