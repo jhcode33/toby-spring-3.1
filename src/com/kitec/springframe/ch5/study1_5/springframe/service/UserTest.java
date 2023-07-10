8cb19cc4f09309898f9a9eeafa09d25cde00cf68
jhcode33 <jhcode33@gmail.com>
2023-07-10 11:26:16 +0900

    add UsetTest

---
+++ src/com/kitec/springframe/ch5/study1_5/springframe/service/UserTest.java
@@ -0,0 +1,32 @@
+package com.kitec.springframe.ch5.study1_5.springframe.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.kitec.springframe.ch5.study1_5.springframe.domain.Level;
+import com.kitec.springframe.ch5.study1_5.springframe.domain.User;
+
+public class UserTest {
+	
+	User user;
+	
+	@BeforeEach
+	public void setUp() {
+		user = new User();
+	}
+	
+	@Test()
+	public void upgradeLevel() {
+		Level[] levels = Level.values();
+		
+		for (Level level : levels) {
+			if(level.nextLevel() == null) continue;
+			user.setLevel(level);
+			user.upgradeLevel();
+			assertEquals(user.getLevel(), level.nextLevel());
+		}
+	}
+
+}
