07afc7239841f224b244e482c8de738e2550cf9b
jhcode33 <jhcode33@gmail.com>
2023-07-12 17:20:43 +0900

    add study ch6-3-4

---
+++ src/com/kitec/springframe/ch6/study3_4/springframe/service/UserService.java
@@ -0,0 +1,9 @@
+package com.kitec.springframe.ch6.study3_4.springframe.service;
+
+import com.kitec.springframe.ch6.study3_4.springframe.domain.User;
+
+public interface UserService {
+	void add(User user);
+	void upgradeLevels();
+}
+
