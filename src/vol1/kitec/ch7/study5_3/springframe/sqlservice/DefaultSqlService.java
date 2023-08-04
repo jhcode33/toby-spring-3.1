7ef36e6e7090ab26f21d1ac725235cb04c3f2bd2
jhcode33 <jhcode33@gmail.com>
2023-08-04 09:14:54 +0900

    add study ch7-5-3

---
+++ src/vol1/kitec/ch7/study5_3/springframe/sqlservice/DefaultSqlService.java
@@ -0,0 +1,8 @@
+package vol1.kitec.ch7.study5_3.springframe.sqlservice;
+
+public class DefaultSqlService extends BaseSqlService {
+	public DefaultSqlService() {
+		setSqlReader(new JaxbXmlSqlReader());
+		setSqlRegistry(new HashMapSqlRegistry());
+	}
+}
---
+++ src/vol1/kitec/ch7/study5_3/springframe/sqlservice/SqlReader.java
@@ -0,0 +1,5 @@
+package vol1.kitec.ch7.study5_3.springframe.sqlservice;
+
+public interface SqlReader {
+	void read(SqlRegistry sqlRegistry);
+}
